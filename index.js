d749778530ff5a6f2ce179a84ded7b700d272dba
Harrish <harrishoffical@gmail.com>
2023-05-07 15:23:43 +0530

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++){
    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
    var buttonInnerHTML = this.innerHTML;
    makeSound(buttonInnerHTML);
+   buttonAnimation(buttonInnerHTML);
    });
 }
 
