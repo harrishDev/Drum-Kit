8138e5479aa4a8fcaff316ee7f55ff59c635376e
Harrish <harrishoffical@gmail.com>
2023-05-07 15:35:29 +0530

    complete drum

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ for(var i=0;i<document.querySelectorAll(".drum").length;i++){
 //detecting keyword press
 document.addEventListener("keydown",function(event){
    makeSound(event,key);
-
-})
+   buttonAnimation(event.key);
+   });
+   
 
 
 function makeSound(key){
    switch (key) {
       case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
+         var tom1 = new Audio("sounds/tom-1.mp3");
+         tom1.play();
+         break;
       case "a":
          var tom2 = new Audio("sounds/tom-2.mp3");
          tom2.play();
@@ -46,9 +47,18 @@ function makeSound(key){
          var kick = new Audio("sounds/kick-bass.mp3");
          kick.play();
          break;
-     default:
+      default:
       console.log(buttonInnerHTML);
 
    }
 
+}
+
+function buttonAnimation(currentKey){
+   var activeButton = document.querySelector("."+ currentKey);
+   activeButton.classList.add("pressed");
+
+   setTimeout(function(){
+      activeButton.classList.remove("pressed");
+   },100);
 }
